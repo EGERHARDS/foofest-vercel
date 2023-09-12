b79ee01e8b01b44d8b40fa9ae8b5bea9bbaea0de
EDGXRS <112377259+EGERHARDS@users.noreply.github.com>
2023-09-12 15:26:39 +0200

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
 export const getAvailableSpots = () =>
-  fetch(`http://localhost:6060/available-spots`);
+  fetch(`https://foofest-backend-bd942c7f29e2.herokuapp.com/available-spots`);
 
 export const updateAvailableSpots = (spots) =>
-  fetch(`http://localhost:6060/reserve-spot`, {
+  fetch(`https://foofest-backend-bd942c7f29e2.herokuapp.com/reserve-spot`, {
     method: "PUT",
     headers: { "Content-Type": `application/json` },
     body: JSON.stringify(spots),
   });
+
